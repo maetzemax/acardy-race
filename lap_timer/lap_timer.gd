ee18265a59130f1a9469be060baac951c3e16552
Max Maetze <maetze.max@icloud.com>
2025-11-28 16:03:47 +0100

    Added: Save and load local times

--- lap_timer/lap_timer.gd
+++ lap_timer/lap_timer.gd
@@ -61,8 +61,9 @@ func _on_lap_completed(crossing_vehicle: Node3D):
 				
 				if current_lap_time < laptime_service.best_lap_time or not laptime_service.best_lap_time:
 					laptime_service.best_lap_time = current_lap_time
-					laptime_service.set_leaderboard_laptime(current_lap_time)
-		
+				
+				laptime_service.set_leaderboard_laptime(current_lap_time)
+			
 			laptime_service.last_lap_time = current_lap_time
 		
 		current_lap_time = 0
