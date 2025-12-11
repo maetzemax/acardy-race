7555e681932ec70ae0a1ab3a1baf17922a2138b3
Max Maetze <maetze.max@icloud.com>
2025-12-11 04:14:35 +0100

    Display Sectors properly

--- ui/main_menu/leaderboard_ui.gd
+++ ui/main_menu/leaderboard_ui.gd
@@ -18,15 +18,23 @@ func _ready():
 		return
 	
 	for entry in entries:
+		var lap_entry_box = VBoxContainer.new()
+		
 		var label = Label.new()
-		label.text = entry.username + " - " + _format_time(entry.laptime) + " (" + _format_sectors(entry.sector_times) + ")"
+		label.text = entry.username + " - " + _format_time(entry.laptime)
+		label.add_theme_font_size_override("font_size", 32)
 		
 		if entry.username == RacingNakamaClient.user.username:
 			label.add_theme_color_override("font_color", Color.GREEN)
 		
-		label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
-		label.add_theme_font_size_override("font_size", 48)
-		lap_time_list.add_child(label)
+		lap_entry_box.add_child(label)
+		
+		var footnote_label = Label.new()
+		footnote_label.text = _format_sectors(entry.sector_times)
+		footnote_label.add_theme_color_override("font_color", Color.LIGHT_GRAY)
+		lap_entry_box.add_child(footnote_label)
+		
+		lap_time_list.add_child(lap_entry_box)
 
 
 func _format_time(time_seconds: float) -> String:
@@ -44,5 +52,5 @@ func _format_sectors(sectors) -> String:
 		
 		if sector != sectors[sectors.size() - 1]:
 			base += ", "
-			
+	
 	return base
